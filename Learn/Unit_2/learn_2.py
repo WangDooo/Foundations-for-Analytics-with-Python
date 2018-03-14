2817591863e48aa652b241ea2684e5e760fef8d8
WangDooo <wangbc1993@163.com>
2018-03-14 22:22:11 +0800

    1

--- Learn/Unit_2/learn_2.py
+++ Learn/Unit_2/learn_2.py
@@ -1,9 +1,25 @@
 # 第二章 CSV文件
 # comma-separated value 逗号分隔值
 
-#================================================================
+#=======读写CSV文件=========================================================
 # 
 #----------------------------------------------------------------
+input_file = 'supplier_data.csv'
+output_file = 'output_1.csv'
+with open(input_file,'r',newline='') as filereader:
+	with open(output_file,'w',newline='') as filewriter:
+		header = filereader.readline()
+		header = header.strip()
+		header_list = header.split(',')
+		print(header_list)
+		filewriter.write(','.join(map(str,header_list))+'\n')
+		# map() 将str()应用于 header_list中每个元素 确保每个元素都是字符串
+		# join() 在header_list中每个值之间插一个逗号 将列表转换为一个字符串
+		for row in filereader:
+			row = row.strip()
+			row_list = row.split(',')
+			print(row_list)
+			
 
 #----------------------------------------------------------------
 
